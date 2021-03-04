d5eddd2aba2fed9c371f910ab6128218552fd2f8
Rysbai <rysbai.coder@gmail.com>
2021-03-04 21:41:52 +0600

    Finished work

--- src/configs.ts
+++ src/configs.ts
@@ -1,5 +1,5 @@
 export default {
-    DB_URL: process.env.DB_URL || 'postgres://django_user:django_password@localhost:5432/bot_shop',
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'rysbai_coder',
+    DB_URL: process.env.DB_URL || 'postgres://bot_db_user:bot_db_password@localhost:5432/bot_shop',
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'kubernurs',
     BOT_TOKEN: process.env.BOT_TOKEN || '1661513781:AAFDRh19B_brVpLhAjmRPi--8VALIOXvpfw'
 }
